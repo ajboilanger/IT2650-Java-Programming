3b8b9ac2398498fd6406dd47e4bdd2342accb5ae
Arthur Boilanger <ajboilanger@gmail.com>
2024-02-18 12:37:08 -0500

    Updated DogTriathlonParticipant exercise

--- Week4/Week4Exercises/DogTriathlonParticipant.java
+++ Week4/Week4Exercises/DogTriathlonParticipant.java
@@ -8,7 +8,7 @@ public class DogTriathlonParticipant
     private int agilityScore;
     private int total;
     private double avg;
-}
+
 
 public DogTriathlonParticipant(String name, int numEvents, int score1, int score2, int score3)
 {
@@ -26,4 +26,5 @@ public void display()
 {
     System.out.println(name + " participated in " + NUM_EVENTS + " events and has an average score of " + avg);
     System.out.println(" " + name + " has a total score of " + total + " bringing the total cumulative score to " + totalCumulativeScore);
+}
 }
